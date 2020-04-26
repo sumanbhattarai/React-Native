2f41a5050fc3942efaffe3715a54bfc71860ee7f
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-04-26 18:00:35 +0545

    BottomTabNavigator added

---
+++ personalApp/src/Home.js
@@ -0,0 +1,73 @@
+import * as React from 'react'
+import { Text, View  , StatusBar} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHome , faSuitcase , faEnvelope , faEdit } from '@fortawesome/free-solid-svg-icons'
+import HomeScreen from './HomeScreen' 
+import PortfolioScreen from './PortfolioScreen'
+import BlogScreen from './BlogScreen'
+import ContactScreen from './ContactScreen'
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#ffffff"
+      barStyle={{ backgroundColor : '#202947'}}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            // <MaterialCommunityIcons name="home" color={color} size={26} />
+            <FontAwesomeIcon icon={ faHome } color={ color } style={{fontSize : 50}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Portfolio"
+        component={PortfolioScreen}
+        options={{
+          tabBarLabel: 'Portfolio',
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={ faSuitcase } color={ color } style={{fontSize : 50}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Blog"
+        component={BlogScreen}
+        options={{
+          tabBarLabel: 'Blog',
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faEdit} color = {color } style={{fontSize : 50}} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={ContactScreen}
+        options={{
+          tabBarLabel: 'Contact',
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faEnvelope} color = {color } style={{fontSize : 50}} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function Home() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#081232" barStyle="light-content" />
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
