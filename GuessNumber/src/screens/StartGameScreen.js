571559c57a51b86b58d8b051cb848b18c91202a8
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-06-06 16:00:27 +0545

    Keyboard Dismiss when touched somewhere

--- GuessNumber/src/screens/StartGameScreen.js
+++ GuessNumber/src/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React  , { useState } from 'react'
-import { View , Text , Button , StyleSheet, TextInput } from 'react-native'
+import { View , Text , Button , StyleSheet, TouchableWithoutFeedback , Keyboard  } from 'react-native'
 import CardDesign from '../components/CardDesign'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
@@ -8,28 +8,32 @@ export default function StartGameScreen(){
     const [enteredValue , setEnteredValue ] = useState('')
 
     return(
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game !</Text>
-            <CardDesign style={styles.inputContainer}>
-                <Text>Select a Number.</Text>
-                <Input 
-                    style={styles.input} 
-                    keyboardType="number-pad"
-                    blurOnSubmit
-                    maxLength={2}
-                    onChangeText={el=>setEnteredValue(el.replace(/[^0-9]/g, ''))}
-                    value={enteredValue}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.buttonView}>
-                        <Button title="Reset" color={Colors.danger}/>
+        <TouchableWithoutFeedback
+            onPress={()=>Keyboard.dismiss()}
+        >
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game !</Text>
+                <CardDesign style={styles.inputContainer}>
+                    <Text>Select a Number.</Text>
+                    <Input 
+                        style={styles.input} 
+                        keyboardType="number-pad"
+                        blurOnSubmit
+                        maxLength={2}
+                        onChangeText={el=>setEnteredValue(el.replace(/[^0-9]/g, ''))}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.buttonView}>
+                            <Button title="Reset" color={Colors.danger}/>
+                        </View>
+                        <View style={styles.buttonView}>
+                            <Button title="Confirm" color={Colors.success} />
+                        </View>
                     </View>
-                    <View style={styles.buttonView}>
-                        <Button title="Confirm" color={Colors.success} />
-                    </View>
-                </View>
-            </CardDesign>
-        </View>
+                </CardDesign>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
