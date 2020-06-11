628df9f29c7659d5e1de907293e76d95a4d0070d
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2020-06-11 17:15:26 +0545

    removed a project

--- StatusApp/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import BottomTabNavigation from './navigation/BottomTabNavigation'
-
-export default function(){
-  return <BottomTabNavigation/>
-}
--- StatusApp/src/constants/default.js
+++
@@ -1,9 +0,0 @@
-const Font =  {
-    primary : 'MuseoSans-500'
-}
-
-const Color = { 
-    primary : '#3E92CC'
-}
-
-export  { Font , Color}
